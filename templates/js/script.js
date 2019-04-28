696867462611e7437db7ddc9d3041d48e1bd0206
JimeBB <jimeberbau@outlook.es>
2019-04-28 09:07:23 -0500

    css update

---
+++ templates/js/script.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("2-bg").classList.toggle("change-bg");
+}
